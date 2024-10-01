87672a064ed3d6bace166a11df40afd01d94f21a
DefinitelyNotMifu <nmpmifu@gmail.com>
2024-10-01 15:22:19 +0700

    news commit

--- backend/models/dataModel.js
+++ backend/models/dataModel.js
@@ -25,10 +25,46 @@ const dataSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    author: {
+        type: String,
+    },
+    comments: [],
 });
 
 dataSchema.plugin(AutoIncrement, { inc_field: "id", start_seq: 1 });
 
-const Data = mongoose.model("Data", dataSchema);
+const commentSchema = new mongoose.Schema({
+    BlogId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Data",
+        required: true,
+    },
+    Id: {
+        type: Number,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    summary: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    comments: [commentSchema],
+});
 
-module.exports = Data;
+commentSchema.plugin(AutoIncrement, { inc_field: "Id", start_seq: 1 });
+
+const Data = mongoose.model("Data", dataSchema);
+const Comments = mongoose.model("Comment", commentSchema);
+
+module.exports = { Data, Comments };
